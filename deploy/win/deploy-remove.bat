cedf5d2513a4b6f11bd385fd85f17cbb8a550773
RexYu <yu.chenhang@gmail.com>
2021-09-22 16:41:01 +0800

    1. ingress 設定併入 deployment.yaml 2. deploy-remove.bat 增加刪除 docker image

--- deploy/win/deploy-remove.bat
+++ deploy/win/deploy-remove.bat
@@ -2,6 +2,14 @@
 
 @set K8S_DEPLOY_HOME=%PROJECT_HOME%/k8s-tester/deploy
 
-call kubectl delete -f %K8S_DEPLOY_HOME%/ingress-nginx.yaml
 call kubectl delete -f %K8S_DEPLOY_HOME%/win/backend-deployment.yaml
 call kubectl delete -f %K8S_DEPLOY_HOME%/win/frontend-deployment.yaml
+
+@REM 暫停3秒
+timeout 3 > NUL
+
+@REM 刪除 image
+call docker rmi k8s-frontend:v1
+call docker rmi chenhang0817/k8s-frontend:v1
+call docker rmi k8s-backend:v1
+call docker rmi chenhang0817/k8s-backend:v1
