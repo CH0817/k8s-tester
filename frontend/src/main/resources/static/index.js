0971eb8645281445c3549a8e0a64c7abdee96448
RexYu <yu.chenhang@gmail.com>
2021-11-09 09:55:02 +0800

    移動 JS function 到 index.js

--- frontend/src/main/resources/static/index.js
+++ frontend/src/main/resources/static/index.js
@@ -1,5 +1,59 @@
-function testRequest() {
-    $.get('http://localhost:8081/backend/test/hello/rex', data => {
-        alert(data);
+function directRequestToK8S() {
+    console.info(BACKEND_SERVER);
+    $.ajax({
+        url: BACKEND_SERVER + '/test/hello/' + getUsername(),
+        timeout: 3000,
+        xhrFields: {withCredentials: true},
+        success: showSuccess,
+        error: showError
     });
+}
+
+function requestFromBackendIngress() {
+    $.ajax({
+        url: '/test/k8s/' + getUsername(),
+        timeout: 3000,
+        xhrFields: {withCredentials: true},
+        success: showSuccess,
+        error: showError
+    });
+}
+
+function requestIngress() {
+    $.ajax({
+        url: 'http://backend.k8s.tester.com.tw/test/hello/' + getUsername(),
+        timeout: 3000,
+        xhrFields: {withCredentials: true},
+        success: showSuccess,
+        error: showError
+    });
+}
+
+function requestGateway() {
+    console.info(GATEWAY_SERVER);
+    $.ajax({
+        url: GATEWAY_SERVER + '/back/test/hello/' + getUsername(),
+        timeout: 3000,
+        xhrFields: {withCredentials: true},
+        success: showSuccess,
+        error: showError
+    });
+}
+
+function getUsername() {
+    let username = $('#username').val();
+    if (!username) {
+        username = 'rex';
+    }
+    return username;
+}
+
+function showSuccess(data) {
+    alert(data);
+    console.info(document.cookie);
+}
+
+function showError(jqXHR) {
+    console.info(jqXHR.responseText);
+    console.error(jqXHR);
 }
