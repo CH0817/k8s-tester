60a5209894a0c0032b51f10604e3aa75b864001d
RexYu <yu.chenhang@gmail.com>
2022-01-21 15:30:04 +0800

    刪除無用程式

--- backend/src/main/java/tw/com/rex/k8stester/backend/web/controller/TestController.java
+++ backend/src/main/java/tw/com/rex/k8stester/backend/web/controller/TestController.java
@@ -2,7 +2,6 @@ package tw.com.rex.k8stester.backend.web.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,45 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 import tw.com.rex.k8stester.backend.service.ApiService;
 import tw.com.rex.k8stester.backend.web.request.Name;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
-
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
     private final Logger logger = LoggerFactory.getLogger(TestController.class);
-    @Autowired
-    private ApiService apiService;
+    private final ApiService apiService;
+
+    public TestController(ApiService apiService) {
+        this.apiService = apiService;
+    }
 
     @GetMapping("/hello/{username}")
-    public HttpEntity<String> hello(@PathVariable("username") String username, HttpServletRequest request,
-                                    HttpServletResponse response) {
+    public HttpEntity<String> hello(@PathVariable("username") String username) {
         logger.info("username: {}", username);
-        showCookies(request);
-        response.addCookie(getTestCookie());
         return ResponseEntity.ok(apiService.sayHello(new Name(username)));
-    }
-
-    private void showCookies(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            logger.info("backend have cookies:");
-            Arrays.stream(cookies)
-                  .map(c -> c.getName() + "=" + c.getValue())
-                  .forEach(c -> logger.info("cookie: {}", c));
-        }
-    }
-
-    private Cookie getTestCookie() {
-        Cookie cookie = new Cookie("backend", "backend");
-        cookie.setMaxAge(7 * 24 * 60 * 60);
-        cookie.setSecure(false);
-        cookie.setHttpOnly(true);
-        cookie.setPath("/");
-        return cookie;
     }
 
 }
