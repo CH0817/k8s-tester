3f7c6e6d13e394dc0e22aa2905681ea803788da7
RexYu <yu.chenhang@gmail.com>
2022-01-22 15:29:58 +0800

    feign name、url 使用參數設定

--- backend/src/main/java/tw/com/rex/k8stester/backend/service/ApiService.java
+++ backend/src/main/java/tw/com/rex/k8stester/backend/service/ApiService.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import tw.com.rex.k8stester.backend.web.request.Name;
 
-@FeignClient(name = "k8s-api-service")
+@FeignClient(name = "${k8s.service-id.api}", url = "${k8s.url.api}")
 public interface ApiService {
 
     @PostMapping(value = "/api/say", produces = {MediaType.APPLICATION_JSON_VALUE})
